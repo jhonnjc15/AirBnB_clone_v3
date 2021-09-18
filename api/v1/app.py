750fdc42c3edcf492703d65e08185bcdb223948c
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-18 12:19:40 +0000

    Starting the Api with the firt endpoint using blueprint

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    After each request, this method calls .close()
+    """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Route that return the status
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def status():
+    """
+    function for status route that returns the status
+    """
+    response = {
+               "status": "OK"
+               }
+    return jsonify(response)
