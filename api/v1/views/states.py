9ee0055b8ce8b486aef264c5e55b3a2f6798ede8
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-20 08:03:42 +0000

    Creating a new view for State objects that handles all default RESTFul API actions

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,17 +5,56 @@ from models import storage
 from models.state import State
 from api.v1.views import app_views
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     states = []
     for state in storage.all("State").values():
         states.append(state.to_dict())
-    return jsonify(state)
+    return jsonify(states)
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
-def get_states(state_id):
-    state = storage.get("State", state_id)
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_states_by_id(state_id):
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state_by_id(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    state.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state_by_id():
+    if request.get_json() is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    state = State(**request.get_json())
+    state.save()
+    return make_response(jsonify(state.to_dict()), 201)
+
+
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_state_by_id(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    for a, v in request.get_json().items():
+        if a not in ['id', 'created_at', 'updated_at']:
+            setattr(state, a, v)
+    state.save()
     return jsonify(state.to_dict())
