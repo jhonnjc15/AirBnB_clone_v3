fd90f532c960c7e3797465ecc9c2ae8725208eda
joseheco <jferhc@gmail.com>
2021-09-20 06:51:58 -0500

    delete make_response

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """cities"""
 
-import re
 from flask import abort, jsonify, make_response, request
 from models import storage
-from models.state import State
 from api.v1.views import app_views
+from models.state import State
 from models.city import City
 
 
@@ -19,7 +18,7 @@ def get_cities(state_id):
     cities = []
     for city in state.cities:
         cities.append(city.to_dict())
-    return make_response(jsonify(cities))
+    return jsonify(cities)
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'],
@@ -29,7 +28,7 @@ def get_cities(city_id):
     city = storage.get("City", city_id)
     if city is None:
         abort(404)
-    return make_response(jsonify(city.to_dict()))
+    return jsonify(city.to_dict())
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'],
@@ -41,7 +40,7 @@ def delete_city(city_id):
         abort(404)
     city.delete()
     storage.save()
-    return make_response(jsonify({}))
+    return jsonify({})
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
@@ -75,4 +74,4 @@ def put_city(city_id):
         if a not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, a, v)
     city.save()
-    return make_response(jsonify(city.to_dict()), 201)
+    return jsonify(city.to_dict()), 200
