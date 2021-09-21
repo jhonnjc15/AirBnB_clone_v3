edc177972489cc17fe4ca30af290148d63d5b204
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-21 04:55:43 +0000

    Create a new view for the link between Place objects and Amenity objects

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -6,17 +6,26 @@ from models import storage
 from models.place import Place
 from models.amenity import Amenity
 from api.v1.views import app_views
+from os import environ
+
+STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
 
 
 @app_views.route('/places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
 def get_amenities(place_id):
     """get amenity information"""
-    place = storage.get_place(Place, place_id)
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    amenities = [ameni.to_dict() for ameni in place.amenities]
-    return jsonify(amenities)
+    place_amenities = []
+    if STORAGE_TYPE == 'db':
+        place_amenities_obj = place.amenities
+    else:
+        place_amenities_obj = place.amenities_ids
+    for i in place_amenities_obj:
+        place_amenities.append(i.to_dict())
+    return jsonify(place_amenities)
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
@@ -29,7 +38,16 @@ def delete_amenitiy(place_id, amenity_id):
         abort(404)
     if amenity is None:
         abort(404)
-    
+    if STORAGE_TYPE == 'db':
+        place_amenities_obj = place.amenities
+    else:
+        place_amenities_obj = place.amenities_ids
+    if amenity not in place_amenities_obj:
+        abort(404)
+    place_amenities_obj.remove(amenity)
+    place.save()
+    return jsonify({})
+
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['POST'], strict_slashes=False)
@@ -40,3 +58,12 @@ def post_amenity(place_id, amenity_id):
         abort(404)
     if amenity is None:
         abort(404)
+    if STORAGE_TYPE == 'db':
+        place_amenities_obj = place.amenities
+    else:
+        place_amenities_obj = place.amenity_ids
+    if amenity in place_amenities_obj:
+        return jsonify(amenity.to_dict())
+    place_amenities_obj.append(amenity)
+    place.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
